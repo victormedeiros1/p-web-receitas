fc2583fe57153d267e10aef943f7acf4e932b51c
victormedeiros1 <josevqczt@gmail.com>
2022-09-14 16:08:16 -0300

    fix: change let to const

--- js-arrow/main.js
+++ js-arrow/main.js
@@ -1,10 +1,10 @@
 const tbody = document.querySelector('tbody');
 const buttonLoadBeers = document.querySelector('.button-load-beers');
 
-let beers = ['Heineken', 'Guiness', 'Desesperados', 'Becks'];
+const beers = ['Heineken', 'Guiness', 'Desesperados', 'Becks'];
 buttonLoadBeers.addEventListener('click', () => loadBeers(beers));
 
-const loadBeers = () => {
+const loadBeers = (beers) => {
   tbody.innerHTML = '';
   beers.map((beer) => (tbody.innerHTML += `<tr><td>${beer}</td></tr>`));
 };
