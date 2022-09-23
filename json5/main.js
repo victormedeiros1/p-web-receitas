6f124c8f22b08a0a5539586acbca21d4305cd129
victormedeiros1 <josevqczt@gmail.com>
2022-09-23 20:25:17 -0300

    feat: json5 -> Step 4

--- json5/main.js
+++ json5/main.js
@@ -1,17 +1,19 @@
+import { load } from './request.js';
+
 const tbody = document.querySelector('tbody');
 const thead = document.querySelector('thead');
 const buttonLoad = document.querySelector('.button-load');
 const loadingBox = document.querySelector('.loading-box');
 const checkBoxes = document.querySelectorAll('.checkbox');
 
-let baseURL = 'https://random-data-api.com/api/v2/banks?size=3';
+let baseURL = 'https://random-data-api.com/api/v2/blood_types?size=3';
 let sizeRequest = 3;
 let isLoading = false;
 
 const toggleLoading = () => {
   isLoading = !isLoading;
 
-  loadingBox.style.display = isLoading ? 'none' : 'initial';
+  loadingBox.style.display = isLoading ? 'initial' : 'none';
 };
 
 const sendRequest = async () => {
@@ -25,42 +27,18 @@ const sendRequest = async () => {
   }
 };
 
-sendRequest();
-
 const setSizeRequest = (e) => {
-  size = e.target.value;
-  baseURL = `https://random-data-api.com/api/v2/banks?size=${size}`;
-};
-
-const load = async () => {
-  const data = await sendRequest();
-  toggleLoading();
-
-  thead.innerHTML = '';
-
-  const keys = Object.keys(data[0]);
-
-  // ===========================================================================
-
-  let headerItemsArray = keys.map((key) => `<th>${key}</th>`);
-
-  const headerItemsTag = `<tr>${headerItemsArray.join('')}</tr>`;
-
-  thead.innerHTML = headerItemsTag;
-
-  // ===========================================================================
-
-  let keysArray = data.map(
-    (item) => `<tr>${keys.map((key) => `<td>${item[key]}</td>`)}</tr>`
-  );
-
-  const bodyItemsTag = `<tr>${keysArray}</tr>`;
-
-  tbody.innerHTML = bodyItemsTag.replaceAll('>,<', '><');
+  sizeRequest = e.target.value;
+  baseURL = `https://random-data-api.com/api/v2/blood_types?size=${sizeRequest}`;
 };
 
 for (const checkBox of checkBoxes) {
   checkBox.addEventListener('click', (e) => setSizeRequest(e));
 }
 
-buttonLoad.addEventListener('click', () => load());
+buttonLoad.addEventListener('click', async () => {
+  const data = await sendRequest();
+  load(data, thead, tbody, toggleLoading);
+
+  toggleLoading();
+});
