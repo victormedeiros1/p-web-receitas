2951846ad086eb1d97c271439ca2ae9ccf756954
victormedeiros1 <josevqczt@gmail.com>
2022-11-04 16:13:10 -0300

    feat: Step 1

---
+++ unidade2/next-05/pages/movies/index.jsx
@@ -0,0 +1,81 @@
+import useSWR from 'swr';
+import 'bootstrap/dist/css/bootstrap.css';
+import { useState } from 'react';
+import { Button, Card } from 'react-bootstrap';
+
+export default function Movies() {
+  const [url, setUrl] = useState('');
+
+  const { data, error } = useSWR(url, theFetcher);
+
+  const onClickHandler = (e) => {
+    e.preventDefault();
+
+    if (url === '') setUrl('http://www.omdbapi.com/?apikey=1ecf8ec3&s=bagdad');
+    else setUrl('');
+  };
+
+  return (
+    <div>
+      <TheLink url={url} handler={onClickHandler} />
+
+      <TheMovies
+        data={
+          error ? { error: 'Erro na pesquisa' } : data ? data : { Search: '' }
+        }
+        show={url !== ''}
+      />
+    </div>
+  );
+}
+
+async function theFetcher(url) {
+  if (url === null || url === '') return { Search: '' };
+
+  const res = await fetch(url);
+
+  const json = await res.json();
+
+  return json;
+}
+
+export function TheMovies({ data, show }) {
+  if (!show) return <div></div>;
+
+  if (data.error) return <div>falha na requisição</div>;
+
+  if (data.Search === '') return <div>carregando...</div>;
+  console.log(data);
+  return (
+    <div className="d-flex gap-3">
+      {data.Search.map((m) => (
+        <div>
+          <Card>
+            <Card.Img style={{ width: '200px' }} variant="top" src={m.Poster} />
+            <Card.Body>
+              <Card.Title>{m.Title}</Card.Title>
+              <Card.Text>
+                {m.Year} --- Lorem ipsum dolor sit amet, consectetur adipiscing
+                elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export function TheLink({ url, handler }) {
+  return (
+    <div className="m-2">
+      <a href="/movies3.js" onClick={handler}>
+        <button className="btn btn-primary">
+          {url === '' ? 'Mostrar' : 'Ocultar'}
+        </button>
+      </a>
+    </div>
+  );
+}
