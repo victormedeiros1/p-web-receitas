7ae858bf48b2628c613b0c9249909369acde6bf4
victormedeiros1 <josevqczt@gmail.com>
2022-11-04 14:36:21 -0300

    fix: Location Step 2

---
+++ unidade2/next-04/pages/location/index.jsx
@@ -0,0 +1,38 @@
+import useSWR from 'swr';
+
+export default function Location() {
+  const { data, error } = useSWR(
+    'https://extreme-ip-lookup.com/json/?key=kCbsFJdk16GRNvqx4Alz',
+    fetcher
+  );
+
+  if (error) return <div>falha na requisição...</div>;
+  if (!data) return <div>carregando...</div>;
+
+  const p = {
+    borderBottom: '1px solid #ccc',
+    fontFamily: 'Verdana, sans-serif',
+    margin: 0,
+    padding: '4 0',
+  };
+
+  return (
+    <div>
+      {Object.keys(data).map((info) => (
+        <div>
+          <p style={p}>
+            <b>{info}: </b>
+            {data[info]}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+async function fetcher(url) {
+  const res = await fetch(url);
+  const json = await res.json();
+
+  return json;
+}
