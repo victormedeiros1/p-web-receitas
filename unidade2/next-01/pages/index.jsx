bc83d0e2686c786c8fc911632172316ceaa9a7af
victormedeiros1 <josevqczt@gmail.com>
2022-11-18 14:58:54 -0300

    fix: refined functions

--- unidade2/next-01/pages/index.jsx
+++ unidade2/next-01/pages/index.jsx
@@ -4,7 +4,12 @@ import { Failure } from './failure';
 const Home = () => {
   Success();
   Failure();
-  return <h1>Running Next!</h1>;
+  return (
+    <div>
+      <h1>Running NextJS</h1>
+      <h2>Verify functions in console!</h2>
+    </div>
+  );
 };
 
 export default Home;
