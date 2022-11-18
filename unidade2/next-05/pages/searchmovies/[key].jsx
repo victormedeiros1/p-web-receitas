c9319eedb302399e9cddc44056790310259590e1
victormedeiros1 <josevqczt@gmail.com>
2022-11-18 14:54:32 -0300

    feat: Step 3

--- unidade2/next-05/pages/searchmovies/[key].jsx
+++ unidade2/next-05/pages/searchmovies/[key].jsx
@@ -1,64 +1,17 @@
-import useSWR from 'swr';
 import 'bootstrap/dist/css/bootstrap.css';
-import { useState } from 'react';
-import { Card } from 'react-bootstrap';
-import Link from 'next/link';
+import { Button, Card } from 'react-bootstrap';
 
-export default function Movies() {
-  const [url, setUrl] = useState('');
-
-  const { data, error } = useSWR(url, theFetcher);
-
-  const onClickHandler = (e) => {
-    e.preventDefault();
-
-    if (url === '') setUrl('http://www.omdbapi.com/?apikey=1ecf8ec3&s=bagdad');
-    else setUrl('');
-  };
-
+export default function Movies({ data }) {
+  if (data.Error) return <div>Nenhum filme encontrado</div>;
   return (
-    <div>
-      <TheLink url={url} handler={onClickHandler} />
-
-      <TheMovies
-        data={
-          error ? { error: 'Erro na pesquisa' } : data ? data : { Search: '' }
-        }
-        show={url !== ''}
-      />
-    </div>
-  );
-}
-
-async function theFetcher(url) {
-  if (url === null || url === '') return { Search: '' };
-
-  const res = await fetch(url);
-
-  const json = await res.json();
-
-  return json;
-}
-
-export function TheMovies({ data, show }) {
-  if (!show) return <div></div>;
-
-  if (data.error) return <div>falha na requisição</div>;
-
-  if (data.Search === '') return <div>carregando...</div>;
-  console.log(data);
-  return (
-    <div className="d-flex gap-3">
-      {data.Search.map((m) => (
-        <div>
+    <div className="d-flex">
+      {data.Search.map(({ Poster, Title, Year }) => (
+        <div key={Title}>
           <Card>
-            <Card.Img style={{ width: '200px' }} variant="top" src={m.Poster} />
+            <Card.Img style={{ width: '200px' }} variant="top" src={Poster} />
             <Card.Body>
-              <Card.Title>
-                <Link href={`/movie/${m.imdbID}`}>{m.Title}</Link>
-              </Card.Title>
               <Card.Text>
-                {m.Year} --- Lorem ipsum dolor sit amet, consectetur adipiscing
+                {Year} --- Lorem ipsum dolor sit amet, consectetur adipiscing
                 elit, sed do eiusmod tempor incididunt ut labore et dolore magna
                 aliqua.
               </Card.Text>
@@ -70,15 +23,15 @@ export function TheMovies({ data, show }) {
   );
 }
 
-export function TheLink({ url, handler }) {
-  return (
-    <div className="m-2">
-      <Link href="/movies3.js" onClick={handler}>
-        <button className="btn btn-primary">
-          {url === '' ? 'Mostrar' : 'Ocultar'}
-        </button>
-      </Link>
-      <Link href="/searchMovies">Procure por filmes...</Link>
-    </div>
-  );
+export async function getServerSideProps(context) {
+  const { key } = context.query;
+  const res = await fetch(`http://www.omdbapi.com/?apikey=1ecf8ec3&s=${key}`);
+
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
 }
